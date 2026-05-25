7dcf233b79e3335eee724b999e13cac74933104d
rfzhfrn <5026231127@student.its.ac.id>
2026-05-25 11:48:58 +0700

    pertemuan 12

---
+++ resources/views/index.blade.php
@@ -0,0 +1,35 @@
+@extends('template')
+@section('title', 'Data Pegawai')
+@section('konten')
+
+	<a href="/pegawai/tambah" class="btn btn-primary"> + Tambah Pegawai Baru</a>
+
+    <p>Cari Data Pegawai :</p>
+	<form action="/pegawai/cari" method="GET">
+		<input type="text" name="cari" placeholder="Cari Pegawai .." class="form-control">
+		<input type="submit" value="CARI">
+	</form>
+	<table class="table table-striped tabel-hover">
+		<tr>
+			<th>Nama</th>
+			<th>Jabatan</th>
+			<th>Umur</th>
+			<th>Alamat</th>
+			<th>Opsi</th>
+		</tr>
+		@foreach($pegawai as $p)
+		<tr>
+			<td>{{ $p->pegawai_nama }}</td>
+			<td>{{ $p->pegawai_jabatan }}</td>
+			<td>{{ $p->pegawai_umur }}</td>
+			<td>{{ $p->pegawai_alamat }}</td>
+			<td>
+				<a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-warning">Edit</a>
+				|
+				<a href="/pegawai/hapus/{{ $p->pegawai_id }}" class="btn btn-danger">Hapus</a>
+			</td>
+		</tr>
+		@endforeach
+	</table>
+    {{ $pegawai->links() }}
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiDBController;
+use App\Http\Controllers\PegawaiController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,5 +21,13 @@ Route::get('pertemuan5', function () {
 	return view('pertemuan5');
 });
 
+
 Route::get('/dosen', [DosenController::class, 'index']);
 Route::get('/biodata', [DosenController::class, 'biodata']);
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+Route::get('/pegawai/tambah', [PegawaiDBController::class, 'tambah']);
+Route::post('/pegawai/store', [PegawaiDBController::class, 'store']);
+Route::get('/pegawai/edit/{id}', [PegawaiDBController::class, 'edit']);
+Route::post('/pegawai/update', [PegawaiDBController::class, 'update']);
+Route::get('/pegawai/hapus/{id}', [PegawaiDBController::class, 'hapus']);
+Route::get('/pegawai/cari', [PegawaiDBController::class, 'cari']);

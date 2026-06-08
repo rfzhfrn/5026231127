bcd64e2a9f21b5bb8fdd276cceb6fb5ad850d70e
rfzhfrn <5026231127@student.its.ac.id>
2026-06-08 14:48:30 +0700

    Pertemuan 14

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -37,6 +37,9 @@
         <li class="nav-item">
         <a class="nav-link" href="/keranjang">Latihan EAS</a>
         </li>
+        <li class="nav-item">
+         <a class="nav-link" href="/siswa">Siswa</a>
+        </li>
     </ul>
   </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\DosenController;
 use App\Http\Controllers\PegawaiDBController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\KeranjangBelanjaController;
+use App\Http\Controllers\SiswaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -36,3 +37,9 @@ Route::get('/keranjang', [KeranjangBelanjaController::class, 'index']);
 Route::get('/keranjang/tambah', [KeranjangBelanjaController::class, 'create']);
 Route::post('/keranjang/simpan', [KeranjangBelanjaController::class, 'store']);
 Route::get('/keranjang/hapus/{id}', [KeranjangBelanjaController::class, 'destroy']);
+Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa.index');
+Route::get('/siswa/create', [SiswaController::class, 'create'])->name('siswa.create');
+Route::post('/siswa', [SiswaController::class, 'store'])->name('siswa.store');
+Route::get('/siswa/{nrp}/edit', [SiswaController::class, 'edit'])->name('siswa.edit');
+Route::put('/siswa/{nrp}', [SiswaController::class, 'update'])->name('siswa.update');
+Route::delete('/siswa/{nrp}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
